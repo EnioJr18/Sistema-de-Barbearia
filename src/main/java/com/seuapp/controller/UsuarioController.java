a985fe09634750488e3a8802df27c1d5c962a694
Enio Jr <eniojr100@gmail.com>
2026-04-03 20:59:51 -0300

    :sparkles: feat: Implementação de Paginação, Ordenação e Filtros de Buscas

--- src/main/java/com/seuapp/controller/UsuarioController.java
+++ src/main/java/com/seuapp/controller/UsuarioController.java
@@ -4,7 +4,9 @@ import com.seuapp.dto.UsuarioResponseDTO;
 import com.seuapp.model.Usuario;
 import com.seuapp.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,18 +18,29 @@ public class UsuarioController {
 
     private final UsuarioRepository usuarioRepository;
     private final org.springframework.security.crypto.password.PasswordEncoder passwordEncoder;
+
+
     @GetMapping
-    public List<UsuarioResponseDTO> listarTodos() {
-        return usuarioRepository.findAll().stream()
-                .map(usuario -> {
-                    UsuarioResponseDTO dto = new UsuarioResponseDTO();
-                    dto.setId(usuario.getId());
-                    dto.setNome(usuario.getNome());
-                    dto.setEmail(usuario.getEmail());
-                    dto.setPerfil(usuario.getPerfil());
-                    return dto;
-                })
-                .toList();
+    public Page<UsuarioResponseDTO> listarTodos(
+            @PageableDefault(size = 10) Pageable pageable,
+            @RequestParam(required = false) String nome) {
+
+        Page<Usuario> paginaDeUsuarios;
+
+        if (nome != null) {
+            paginaDeUsuarios = usuarioRepository.findByNomeContainingIgnoreCase(nome, pageable);
+        } else {
+            paginaDeUsuarios = usuarioRepository.findAll(pageable);
+        }
+
+        return paginaDeUsuarios.map(usuario -> {
+            UsuarioResponseDTO dto = new UsuarioResponseDTO();
+            dto.setId(usuario.getId());
+            dto.setNome(usuario.getNome());
+            dto.setEmail(usuario.getEmail());
+            dto.setPerfil(usuario.getPerfil());
+            return dto;
+        });
     }
 
     @PostMapping
--- src/main/java/com/seuapp/repository/UsuarioRepository.java
+++ src/main/java/com/seuapp/repository/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package com.seuapp.repository;
 
 import com.seuapp.model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     org.springframework.security.core.userdetails.UserDetails findByEmail(String email);
+
+    Page<Usuario> findByNomeContainingIgnoreCase(String nome, Pageable pageable);
 }
